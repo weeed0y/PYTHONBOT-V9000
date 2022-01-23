95d35949a919bc6417414db06426df3691f27915
【★༒✞︎Îτß-ℓєɠεɳ∂-ℓх】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2022-01-22 23:54:42 -0800

    Update updater.py

--- setup/updater.py
+++ setup/updater.py
@@ -42,7 +42,7 @@ async def update_requirements(main , test):
     try:
         for i in a:
             await runcmd(f"pip install {i}")
-            print(f"🔥🗡 Legend-lx 🔥🗡 {i}")
+            print(f"🔥🗡 DARKLEGEND-Pro 🔥🗡 {i}")
     except Exception as e:
         print(f"Error while installing requirments {str(e)}")
 
