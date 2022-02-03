54ef12a1f7f7c0950153eaabe07104c612d233e9
【★༒✞︎Drak-ℓєɠεɳ∂-Pro】 <98252847+DARK-LEGEND-PRO@users.noreply.github.com>
2022-02-03 07:13:06 -0800

    Update updater.py

--- setup/updater.py
+++ setup/updater.py
@@ -42,7 +42,7 @@ async def update_requirements(main , test):
     try:
         for i in a:
             await runcmd(f"pip install {i}")
-            print(f"🔥🗡 DARKLEGEND-Pro 🔥🗡 {i}")
+            print(f"💥💫PythonBot~Pro💫💥 {i}")
     except Exception as e:
         print(f"Error while installing requirments {str(e)}")
 
